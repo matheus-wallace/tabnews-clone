7cba27c78263509b13825b165f20f8cc038106c3
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-11-16 20:44:43 -0300

    feat: test secret workflow

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -5,7 +5,11 @@ async function status(request, response) {
   const databseVersionResult = await database.query("SHOW server_version;");
   const databseVersionValue = databseVersionResult.rows[0].server_version;
 
-  const AWS_SECRET_ACCESS_KEY = "abc123xyz";
+  const AWS_SECRET_ACCESS_KEY = {
+    Account: "123456789012",
+    UserId: "AR#####:#####",
+    Arn: "arn:aws:sts::123456789012:assumed-role/role-name/role-session-name",
+  };
 
   const maxConnection = await database.query("SHOW max_connections;");
   const maxConnectionsValue = maxConnection.rows[0].max_connections;
