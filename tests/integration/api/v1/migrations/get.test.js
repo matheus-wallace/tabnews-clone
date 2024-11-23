8ef1ddb50e728d1e425dbb49243502b091b1bfd1
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-11-23 19:18:41 -0300

    test: rename all descriptions to user new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,17 +6,21 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("GET to /api/v1/migrations should return and array of objects", async () => {
-  //verify if the response migrations is 200
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retriving pending migrations", async () => {
+      //verify if the response migrations is 200
+      const response = await fetch("http://localhost:3000/api/v1/migrations");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-  expect(responseBody.length).toBe(1);
-  expect(typeof responseBody[0].path).toBe("string");
-  expect(typeof responseBody[0].name).toBe("string");
-  expect(typeof responseBody[0].timestamp).toBe("number");
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+      expect(responseBody.length).toBe(1);
+      expect(typeof responseBody[0].path).toBe("string");
+      expect(typeof responseBody[0].name).toBe("string");
+      expect(typeof responseBody[0].timestamp).toBe("number");
+    });
+  });
 });
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -6,28 +6,43 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public");
 });
 
-test("POST to /api/v1/migrations should return and array of objects", async () => {
-  //verify if the response migrations is 200
-  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("for the first time", async () => {
+        //verify if the response migrations is 200
+        const response1 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response1.status).toBe(201);
+
+        const response1Body = await response1.json();
+
+        expect(Array.isArray(response1Body)).toBe(true);
+        expect(response1Body.length).toBeGreaterThan(0);
+        expect(typeof response1Body[0].path).toBe("string");
+        expect(typeof response1Body[0].name).toBe("string");
+        expect(typeof response1Body[0].timestamp).toBe("number");
+      });
+      test("for second time", async () => {
+        //verify if the response migrations is 200
+
+        const response2 = await fetch(
+          "http://localhost:3000/api/v1/migrations",
+          {
+            method: "POST",
+          },
+        );
+        expect(response2.status).toBe(200);
+
+        const response2Body = await response2.json();
+
+        expect(Array.isArray(response2Body)).toBe(true);
+        expect(response2Body.length).toBe(0);
+      });
+    });
   });
-  expect(response1.status).toBe(201);
-
-  const response1Body = await response1.json();
-
-  expect(Array.isArray(response1Body)).toBe(true);
-  expect(response1Body.length).toBeGreaterThan(0);
-  expect(typeof response1Body[0].path).toBe("string");
-  expect(typeof response1Body[0].name).toBe("string");
-  expect(typeof response1Body[0].timestamp).toBe("number");
-
-  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  expect(response2.status).toBe(200);
-
-  const response2Body = await response2.json();
-
-  expect(Array.isArray(response2Body)).toBe(true);
-  expect(response2Body.length).toBe(0);
 });
